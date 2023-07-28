122158614af46e32c9ac509c616332a983145e4e
eddyhakimi <eddi.hakimi@gmx.de>
2023-07-28 14:21:33 +0200

    Init frontend project with Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Invoice Generator",
+  description: "Generate invoices for your clients",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <main>Invoice generator: Hello World</main>;
+}
